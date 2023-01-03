26d209eed70926e87c99bf5f2d8ef34a198ee783
RecordOfJun <pritty0731@naver.com>
2023-01-03 18:27:45 +0900

    캔버스 레이아웃 잡기

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,14 +1,35 @@
 import React from "react";
+import "./Main.css"
 import FaceMeshCam from "../components/faceMesh/FaseMeshCam";
 import { Canvas } from '../components/canvas/Canvas'
 import { ClearCanvasButton } from '../components/canvas/ClearCanvasButton';
 
 const Main = () => {
     return (
-        <div>
-            <Canvas />
+        <div className="whole-container">
+            <div className="top-bar"> {/*상단 바 div*/}
+                <div className="eyetist-font">
+                    EyeTist
+                </div>
+            </div>
+
+            <div className="components-container">
+                
+                <div className="functions-container">
+
+                </div>
+
+                <div className="canvas-container">
+                    <Canvas />
+                </div>
+
+                <div className="detail-container">
+                    <FaceMeshCam />
+                </div>
+            </div>
+            {/* <Canvas />
             <ClearCanvasButton />
-            <FaceMeshCam />
+            <FaceMeshCam /> */}
         </div>
     )
 }
