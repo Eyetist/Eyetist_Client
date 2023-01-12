3eb681f824f9eb42ab7a7ccfd2ff0eeded9c68c8
DaeWon9 <qkreodnjs97@naver.com>
2023-01-13 04:06:51 +0900

    [REMOVE] gallery button

--- src/components/functionDetails/MoveSelection.js
+++ src/components/functionDetails/MoveSelection.js
@@ -1,7 +1,6 @@
 import EyeButton from "../atoms/EyeButton"
 import { FaPowerOff } from "react-icons/fa"
 import { BiLogOut } from "react-icons/bi"
-import { BsImages } from "react-icons/bs"
 import { useNavigate } from 'react-router-dom';
 
 const TOOL_BUTTON_SIZE = window.innerWidth * 0.03
@@ -21,10 +20,6 @@ const toolButtonStyle = {
 const MoveSelections = (props) => {
     let navigate = useNavigate();
 
-    function goGallery(){
-        navigate('/gallery')
-    }
-
     function goBack(){
         navigate('/begin')
     }
@@ -36,18 +31,6 @@ const MoveSelections = (props) => {
 
     return(
         <div style={{display:"flex"}}>
-            {
-                props.currentPage === "gallery" || props.currentPage === "begin"?
-                <></>
-                :
-                <EyeButton 
-                    style={toolButtonStyle}
-                    text={<BsImages />}
-                    hoverColor="pink"
-                    clickColor="black"
-                    onClick={() => {goGallery()}}
-                />
-            }
             {
                 props.currentPage === "begin" ?
                 <></>
